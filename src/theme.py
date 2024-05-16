b3dfc5cf44ccff86061456fb546c897dd7d2f704
Philipp Hager <philipp.konstantin.hager@gmail.com>
2024-05-16 17:27:09 +0200

    Create theme.py

---
+++ src/theme.py
@@ -0,0 +1,37 @@
+def theme():
+    return {
+        "config": {
+             "title": {
+                "font": "serif",
+                "fontWeight": "normal",
+                "fontSize": 16,
+                "dx": 5
+            },
+            "axis": {
+                "titleFont": "serif",
+                "titleFontWeight": "normal",
+                "titleFontSize": 16,
+                "labelFont": "serif",
+                "labelFontWeight": "normal",
+                "labelFontSize": 16
+            },
+            "headerColumn": {
+                "titleFont": "serif",
+                "titleFontWeight": "normal",
+                "titleFontSize": 16,
+                "labelFont": "serif",
+                "labelFontWeight": "normal",
+                "labelFontSize": 16
+            },
+            "text": {
+                "font": "serif",
+                "fontSize": 14,
+            },
+            "legend": {
+                "labelFont": "serif",
+                "titleFont": "serif",
+                "titleFontSize": 16,
+                "labelFontSize": 16,
+            }
+        },
+    }
