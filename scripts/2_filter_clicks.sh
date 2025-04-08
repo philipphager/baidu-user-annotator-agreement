355d1cac252d47bbcc8031bcf0347ef3c1145dba
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-04-08 21:21:47 +0200

    Update 2_filter_clicks.sh

--- scripts/2_filter_clicks.sh
+++ scripts/2_filter_clicks.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4GB
 #SBATCH --time=02:00
-#SBATCH --array=0-2000%50
+#SBATCH --array=0-1000%50
 
 source ${HOME}/.bashrc
 mamba activate baidu-user-annotator-agreement
